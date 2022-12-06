99efb4ef67cef629b0964f439bdeae1ed23dc87b
Surui Li <lisurui6@gmail.com>
2022-12-06 21:20:08 +0000

    update init imports

--- ccitk/__init__.py
+++ ccitk/__init__.py
@@ -24,24 +24,28 @@ except ModuleNotFoundError:
     __trimesh_installed__ = False
 
 
-from .image import *
+from .image import *  # nibabel scipy
 __all__ = image.__all__
 
 if __mirtk_installed__ and __vtk_installed__:
-    from . import register
-    from . import refine
-    from . import motion
+    from . import register  # mirtk vtk
+    from . import refine  # SimpleITK, vtk, mirtk
+    from . import motion  # mirtk
     __all__ += ["register", "refine", "motion"]
 
 if __torch_installed__:
-    from . import nn
+    from . import nn  # torch
     __all__ += ["nn"]
     if __mirtk_installed__:
             from . import segment
             __all__ += ["segment"]
 
 if __vtk_installed__:
-    from . import landmark
-    from . import mesh
-    # from . import visual # trimesh needed
+    from . import landmark  # vtk nibabel
+    from . import mesh  # vtk
     __all__ += ["landmark", "mesh", "visual"]
+    try:
+        import imageio, trimesh, meshcut, vedo
+    except:
+        from . import visual  # imageio trimesh vtk meshcut vedo
+        __all__ += ["visual"]
